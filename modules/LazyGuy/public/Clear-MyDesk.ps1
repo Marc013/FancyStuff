f1dbeb10f6ed8082da14319f61e31ed31099364a
Marc van Gorp <Marc.van.Gorp@infield-ict.nl>
2021-06-22 07:26:15 +0200

    Removed functions

--- modules/LazyGuy/public/Clear-MyDesk.ps1
+++
@@ -1,7 +0,0 @@
-function Clear-MyDesk {
-    Assert-HelperFunction
-
-    Write-Host "The butler is cleaning your desk. `nPlease be patient for a couple of seconds." -ForegroundColor Magenta
-    Start-Sleep -Seconds 2
-    Write-Host "Thank you for your patience. `nThe butler has done your dirty work ;-)" -ForegroundColor Green
-}
