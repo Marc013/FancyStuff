196fa36691d1dfffa9847498434e6c8efb3ce58e
Marc van Gorp <Marc.van.Gorp@infield-ict.nl>
2021-06-22 07:49:28 +0200

    Removed executable from butler function

--- modules/LazyGuy/private/Invoke-Butler.ps1
+++ modules/LazyGuy/private/Invoke-Butler.ps1
@@ -1,5 +1,5 @@
 function Invoke-Butler {
-    [string]$Assert = azcopy --version
+    [string]$Assert = 'Dummy for troubleshooting purposes'
 
     if ([string]::IsNullOrWhiteSpace($Assert)) {
         throw "`nazcopy not found. `nPlease make sure azcopy is installed and the directory location of the AzCopy executable is added to your system path. `nhttps://docs.microsoft.com/en-us/azure/storage/common/storage-use-azcopy-v10"
